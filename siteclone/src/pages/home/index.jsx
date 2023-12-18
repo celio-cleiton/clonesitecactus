eb7138fa6406045c7b522a57aa42c05ba7c7cfb0
celio-cleiton <cleitonfreelance@gmail.com>
2023-12-17 23:43:22 -0300

    inicio do site clone

---
+++ siteclone/src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../../components/Header/navbar/navbar";
+
+
+
+function home() {
+ 
+    return (
+      <>
+        <Navbar />
+      </>
+    )
+  }
+  
+  export default home;
+  
